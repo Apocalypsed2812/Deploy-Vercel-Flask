5b857314ae6e675f4689d89aad6389cb57c6c614
Apocalypsed2812 <phamhuynhanhtien123@gmail.com>
2023-02-26 19:40:30 +0700

    add project flask

---
+++ static/js/main.js
@@ -0,0 +1,60 @@
+function getBotResponse() {
+    var rawText = $("#textInput").val();
+    var userHtml = '<p class="userText"><span>' + rawText + '</span></p>';
+    $("#textInput").val("");
+    $("#chatbox").append(userHtml);
+    document.getElementById('userInput').scrollIntoView({block: 'start', behavior: 'smooth'});
+    // $.get("/get", { msg: rawText }).done(function(data) {
+    //   var botHtml = '<p class="botText"><span>' + data + '</span></p>';
+    //   $("#chatbox").append(botHtml);
+    //   document.getElementById('userInput').scrollIntoView({block: 'start', behavior: 'smooth'});
+    // });
+
+    // $.ajax({
+    //   url: '/post',
+    //   data: { msg: rawText },
+    //   processData: false,
+    //   contentType: false,
+    //   type: 'POST',
+    //   success: function(data){
+    //     var botHtml = '<p class="botText"><span>' + data + '</span></p>';
+    //     $("#chatbox").append(botHtml);
+    //     document.getElementById('userInput').scrollIntoView({block: 'start', behavior: 'smooth'});
+    //   }
+    // });
+
+    $.post("/post", { msg: rawText }).done(function(data) {
+      var botHtml = '<p class="botText"><span>' + data + '</span></p>';
+      $("#chatbox").append(botHtml);
+      document.getElementById('userInput').scrollIntoView({block: 'start', behavior: 'smooth'});
+    });
+}
+$("#textInput").keypress(function(e) {
+    if(e.which == 13) {
+        getBotResponse();
+    }
+});
+$("#buttonInput").click(function() {
+  getBotResponse();
+})
+
+const body = document.querySelector("body")
+const modeSwitch = document.querySelector(".dark__mode")
+const modeText = document.querySelector(".mode__text")
+const sunMode = document.querySelector(".sun__mode")
+const moonMode = document.querySelector(".moon__mode")
+
+modeSwitch.addEventListener("click", () => {
+  body.classList.toggle("dark")
+
+  if(body.classList.contains("dark")){
+      sunMode.style.display = 'inline-block'
+      moonMode.style.display = 'none'
+      modeText.innerText = "Light Mode"
+  }
+  else{
+      sunMode.style.display = 'none'
+      moonMode.style.display = 'inline-block'
+      modeText.innerText = "Dark Mode"
+  }
+})
